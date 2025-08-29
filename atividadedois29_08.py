5b5a7dc825b46b1192824bf486dec99e060dbb2c
Nates2912 <srakawaiiuwu@gmail.com>
2025-08-29 16:07:29 -0300

    Add files via upload

--- atividadedois29_08.py
+++ atividadedois29_08.py
@@ -1,3 +1,20 @@
+<<<<<<< HEAD
+#Cleaning the terminal
+
+#USANDO OR (OU)
+
+import os 
+os.system("cls")
+
+nota= int (input("Type in the first number: "))
+print("")
+
+#Se nota menor que zero ou maior que dez
+#OR -> Fora do intervalo definido
+if nota < 0 or nota > 10:
+    print("Invalid input.")
+else:
+=======
 #Cleaning the terminal
 
 #USANDO OR (OU)
@@ -12,4 +29,5 @@ print("")
 if nota < 0 or nota > 10:
     print("Invalid input.")
 else:
-    print (f"Grade: {nota}")
+>>>>>>> c7f1a3a53dabc9fb7ba5986d4f5e76fcd31173be
+    print (f"Grade: {nota}")ade: {nota}")
