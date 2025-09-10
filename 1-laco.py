060f7bfacc35c2209af9312a5f7f869302f4e8be
Nates2912 <srakawaiiuwu@gmail.com>
2025-09-10 16:51:50 -0300

    Add files via upload

---
+++ 1-laco.py
@@ -0,0 +1,11 @@
+import os
+os.system ("cls")
+
+# print("I LOVE TV")
+# print("I LOVE TV")
+# print("I LOVE TV")
+# print("I LOVE TV")
+# print("I LOVE TV")
+
+for i in range(5):
+    print("I LOVE TV")
