c8339b99daf944d50820f582b60005ba9f45792d
Nates2912 <srakawaiiuwu@gmail.com>
2025-09-11 16:45:29 -0300

    Add files via upload

---
+++ 4-2-laco-for.py
@@ -0,0 +1,18 @@
+
+import streamlit as st
+import time
+
+st.title("Summin' up!")
+st.header("Type in 5 numbers to sum em' up.")
+
+soma = 0 #para soma  defaultar como 0 no começo
+
+for i in range (1,6):
+    n1 = st.number_input (f"Type in {i}: ", min_value= 2, step = 1)
+    soma = soma + n1 #para a soma ser a mesma coisa do numero
+    #soma += numero
+    
+if st.button("Start"):
+    st.success (f"The sum is: {soma}")
+else:
+    st.info("Input a number.")
