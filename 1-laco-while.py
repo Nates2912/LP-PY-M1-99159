d4fd80dff1ae0b4ed9570f49659e1fa37f41a36c
Nates2912 <srakawaiiuwu@gmail.com>
2025-09-12 15:49:42 -0300

    Add files via upload

---
+++ 1-laco-while.py
@@ -0,0 +1,25 @@
+# import streamlit as st
+# import time
+
+# st.title("LP")
+# st.header("Laços de repetição - While!")
+
+
+# while n1 == 2:
+#     n1 = st.number_input("Digite um número: " , step= 1)
+
+    # while True:
+    # if n1 == 2:
+    #     break
+
+import os
+os.system("cls")
+
+print ("Laço de repetição - While")
+
+while True:
+        n1 = int(input("Digite um número: "))
+        if n1 == 2:
+            break  
+
+print("FIM") 
