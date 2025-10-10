69c5b8d5027809275a4947233885fb73be5a27e6
Nates2912 <srakawaiiuwu@gmail.com>
2025-10-10 03:31:49 +0000

    feat: corregindo em casa

--- Estudo/Diferenciando.py
+++ Estudo/Diferenciando.py
@@ -1,25 +1,24 @@
-# Limpa a tela (opcional)
+
 import os
 os.system("cls")
 
-# Pede uma entrada ao usuário
+
 texto = input("Digite algo: ")
 
-# Listas para guardar o que for número e o que não for
+
 numeros = []
 letras = []
 simbolos = []
 
-# Percorre cada caractere da entrada
+
 for caractere in texto:
-    if caractere.isdigit():   # Verifica se é um número
-        numeros.append(int(caractere))  # Adiciona na lista de números
+    if caractere.isdigit():   
+        numeros.append(int(caractere))
     elif caractere.isalpha(): 
         letras.append(caractere)  
     else:
-         simbolos.append(caractere)     # Adiciona na lista de letras
+         simbolos.append(caractere)  
 
-# Mostra o resultado
-print("Números encontrados:", numeros)
+print("\nNúmeros encontrados:", numeros)
 print("Letras encontradas:", letras)
 print("Símbolos encontrados:", simbolos)
