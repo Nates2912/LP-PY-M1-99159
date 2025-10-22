ea1d68ba1db47e3982ec6cd05abe74db1c2a4eb7
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-22 16:50:58 -0300

    feat: aeee finalmente codando3

--- 10.Classes/classes.py
+++ 10.Classes/classes.py
@@ -11,18 +11,20 @@ from dataclasses import dataclass
 class Pessoa:
     nome: str
     idade: int
+    cpf: str
 @dataclass
 class Pet:
     nome: str
     idade: int
+    peso: str
 
 #Exemplo de uso da classe
-pessoa1 = Pessoa("Alice",30)
-pet1 = Pet("Bob",4)
+pessoa1 = Pessoa("Alice",30,"086.756.066.32")
+pet1 = Pet("Bob",4, "5kg")
 
-print("Pessoa:")
-print(f"Nome: {pessoa1.nome}\nIdade: {pessoa1.idade}")
+
+print(f"Nome: {pessoa1.nome}\nIdade: {pessoa1.idade}\nCPF: {pessoa1.cpf}")
 
 print("Pet:")
-print(f"Nome: {pet1.nome}\nIdade: {pet1.idade}")
+print(f"Nome: {pet1.nome}\nIdade: {pet1.idade}\nPeso: {pet1.peso}")
 
