50cc8b01654b5a45986d26cacadfd4b05d0d02af
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-22 16:28:34 -0300

    feat: aeee finalmente codando3

--- 10.Classes/classes.py
+++ 10.Classes/classes.py
@@ -12,5 +12,5 @@ class Pessoa:
 pessoa1 = Pessoa("Alice",30)
 pessoa2 = Pessoa("Bob",25)
 
-print(f"Nome: {pessoa1.nome}, Idade: {pessoa1.idade}")
-print(f"Nome: {pessoa2.nome}, Idade: {pessoa2.idade}")
+print(f"Nome: {pessoa1.nome}\nIdade: {pessoa1.idade}")
+print(f"Nome: {pessoa2.nome}\nIdade: {pessoa2.idade}")
