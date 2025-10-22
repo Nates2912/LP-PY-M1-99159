e232e131a05a7eec9e72d30e9595812f0131df57
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-22 16:36:04 -0300

    feat: aeee finalmente codando3

--- 10.Classes/classes.py
+++ 10.Classes/classes.py
@@ -3,14 +3,26 @@ os.system("cls")
 
 from dataclasses import dataclass
 
+#estrutura de dados - classe
+#cada classe tem seu proprio class
+#usado para usar os mesmos nomes para variaveis
+
 @dataclass
 class Pessoa:
+    nome: str
+    idade: int
+@dataclass
+class Pet:
     nome: str
     idade: int
 
 #Exemplo de uso da classe
 pessoa1 = Pessoa("Alice",30)
-pessoa2 = Pessoa("Bob",25)
+pet1 = Pet("Bob",4)
 
+print("Pessoa:")
 print(f"Nome: {pessoa1.nome}\nIdade: {pessoa1.idade}")
-print(f"Nome: {pessoa2.nome}\nIdade: {pessoa2.idade}")
+
+print("Pet:")
+print(f"Nome: {pet1.nome}\nIdade: {pet1.idade}")
+
