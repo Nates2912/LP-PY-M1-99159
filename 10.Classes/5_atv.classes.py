14774f2c1813cd38bab6043de43a8a6f4c09476d
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-23 16:44:43 -0300

    feat: 23/10/20205

--- 10.Classes/5_atv.classes.py
+++ 10.Classes/5_atv.classes.py
@@ -11,28 +11,29 @@ class Pessoa:
     email: str
     endereco: str
    
-    #pegando os dados da classe abaixo com self
-
     def mostrar_dados(self):
-        print("Exibindo os dados pessoais")
+       
         print(f"Nome: {self.nome}\nEndereço: {self.endereco}\nE-mail: {self.email}")
     def mostrar_nome(self):
-        print("Exibindo o nome")
+        
         print(f"Nome: {self.nome}")
 
 #Da pra fazer varias variaveis com as mesmas funçoes com isso
 print("Solicitando os dados da pessoa")
-pessoa1 = Pessoa(nome=input("Digite seu nome: "),
-                 email=input("Digite sua e-mail: "),
-                 endereco=input("Digite sua endereço: "))
-                 
-pessoa2 = Pessoa(nome=input("Digite seu nome: "),
-                 email=input("Digite sua e-mail: "),
-                 endereco=input("Digite sua endereço: "))
-                 
+#vetor!!!
+lista_pessoas = []
+for i in range(2):
+    pessoa = Pessoa(nome=input("Digite seu nome: "),
+                    email=input("Digite sua e-mail: "),
+                    endereco=input("Digite sua endereço: "))
+    lista_pessoas.append(pessoa)
+
 os.system("cls")
 
-pessoa1.mostrar_dados()
-pessoa1.mostrar_nome()
-pessoa2.mostrar_dados()
-pessoa2.mostrar_nome()
+print("\nExibindo os dados pessoais")
+for pessoa in lista_pessoas:
+    pessoa.mostrar_dados()
+
+print("\nExibindo o nome")
+for pessoa in lista_pessoas:
+    pessoa.mostrar_nome()
