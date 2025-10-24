a170e83051337359b5bac41fe59e5087b7ab1a44
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-24 16:09:05 -0300

    feat: 24/10/2025

--- 10.Classes/Atividades/7_atv.classe.py
+++ 10.Classes/Atividades/7_atv.classe.py
@@ -18,5 +18,5 @@ class Pessoa:
 pessoa1 = Pessoa(nome=input("Digite seu nome: "), email= input ("Digite seu e-mail: "),
                 endereco= Endereco (input("Digite seu endereço: "), numero=input("Digite o número da sua casa: "), cidade= input("Digite sua cidade: ")))
 
-print(f"Nome: {pessoa1.nome}, E-mail: {pessoa1.email}, Endereço: {pessoa1.endereco.logradouro}, Número: {pessoa1.endereco.numero}, Cidade: {pessoa1.endereco.cidade}")
+print(f"\nNome: {pessoa1.nome}\n E-mail: {pessoa1.email}\n Endereço: {pessoa1.endereco.logradouro}\n Número: {pessoa1.endereco.numero}\n Cidade: {pessoa1.endereco.cidade}")
 
