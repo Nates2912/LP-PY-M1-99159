c80c7369c97e892d431785657c936d73ab90a77f
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-24 16:25:55 -0300

    feat: 24/10/2025

--- 10.Classes/Atividades/7_atv.classe.py
+++ 10.Classes/Atividades/7_atv.classe.py
@@ -15,8 +15,11 @@ class Pessoa:
     email: str
     endereco : Endereco
 
+    def mostrar_dados(self):
+        print(f"\n==CLIENTE==\n Nome: {self.nome}\n E-mail: {self.email}\n==ENDEREÇO==\n Logadouro:: {self.endereco.logradouro}\n Número: {self.endereco.numero}\n Cidade: {self.endereco.cidade}")
+
 pessoa1 = Pessoa(nome=input("Digite seu nome: "), email= input ("Digite seu e-mail: "),
                 endereco= Endereco (input("Digite seu endereço: "), numero=input("Digite o número da sua casa: "), cidade= input("Digite sua cidade: ")))
 
-print(f"\nNome: {pessoa1.nome}\n E-mail: {pessoa1.email}\n==ENDEREÇO==\n Logadouro: {pessoa1.endereco.logradouro}\n Número: {pessoa1.endereco.numero}\n Cidade: {pessoa1.endereco.cidade}")
+pessoa1.mostrar_dados()
 
