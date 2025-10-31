8361d3d8887e7fb92c42cd8ea491dcf79dc27bbf
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-10-31 16:02:28 -0300

    feat: halloween

--- 10.Classes/Exemplos/exemplo.py
+++ 10.Classes/Exemplos/exemplo.py
@@ -9,6 +9,7 @@ texto = input("Nome: ")
 nome_arquivo = "exemplo.txt"
 
 #comandos para salvar
-with open(nome_arquivo, "w") as meu_arquivo:
-    meu_arquivo.write(texto) 
+# with open(nome_arquivo, "w") as meu_arquivo: # apaga o que foi salvo anteriormente e troca pelo texto atual
+with open(nome_arquivo, "a") as meu_arquivo: # O "a" adiciona e e nao apaga o antigo
+    meu_arquivo.write(f"{texto}\n") 
     print("Salvo com sucesso!")
--- 10.Classes/Exemplos/salvando_em_arquivos.py
+++ 10.Classes/Exemplos/salvando_em_arquivos.py
@@ -17,5 +17,10 @@ for i in range (3):
     alunos = Aluno (nome= input("Digite seu nome: "), idade= int(input("Digite seu idade: ")))
     lista_alunos.append(alunos)
 
-for alunos in lista_alunos:
-    alunos.mostrar_dados()
+print("Salvando dados")
+arquivo = "dados_alunos.txt"
+
+with open(arquivo, "a") as arquivo_alunos: # O "a" adiciona e e nao apaga o antigo
+    for alunos in lista_alunos:
+        arquivo_alunos.write(f"{alunos.nome}, {alunos.idade}\n") 
+    print("Salvo com sucesso!")
