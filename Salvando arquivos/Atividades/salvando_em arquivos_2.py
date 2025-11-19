154d608230c486a534c8c0f7c19397ace9ceda68
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-11-19 15:57:25 -0300

    feat: 19/11/2025

--- Salvando arquivos/Atividades/salvando_em arquivos_2.py
+++ Salvando arquivos/Atividades/salvando_em arquivos_2.py
@@ -8,7 +8,7 @@ class Paciente:
     idade: str
 
     def exibir_dados(self):
-        print(f"Nome: {self.nome} \nIdade: {self.idade} ")
+        print(f"Nome: {self.nome} \nIdade: {self.idade} \n")
 
 lista_de_pacientes = []
 QUANTIDADE_PACIENTES = 2
@@ -19,7 +19,7 @@ for i in range (QUANTIDADE_PACIENTES):
     )
     lista_de_pacientes.append(paciente)
 
-    nome_do_arquivo = "dados_pacientes.csv"
+    nome_do_arquivo = "dados_pacientes2.csv"
     with open(nome_do_arquivo, "a") as arquivos_pacientes:
         for paciente in lista_de_pacientes:
             arquivos_pacientes.write(f"{paciente.nome}, {paciente.idade}")
@@ -31,13 +31,29 @@ for i in range (QUANTIDADE_PACIENTES):
 
 #csv e mais usado para dados
 
-print("\nExibindo todos os pacientes:")
-try:
-    #"r" read - leitura
+# print("\nExibindo todos os pacientes:")
+# try:
+#     #"r" read - leitura
+#     with open (nome_do_arquivo, "r") as arquivo:
+#         #RECEBE TODOS OS DADOS DE UM ARQUIVO DE UMA SO VEZ
+#         lista_de_pacientes  = arquivo.readlines()
+#         for paciente in lista_de_pacientes:
+#             print(f"-{paciente.strip()}")
+# except FileNotFoundError: 
+#     print("O arquivo não foi encontrado.")
+
+print("\nExibindo todos os pacientes: ")
+lista = []
+try: 
     with open (nome_do_arquivo, "r") as arquivo:
-        #RECEBE TODOS OS DADOS DE UM ARQUIVO DE UMA SO VEZ
-        lista_de_pacientes  = arquivo.readlines()
+        lista_de_pacientes = arquivo.readlines()
         for paciente in lista_de_pacientes:
-            print(f"-{paciente.strip()}")
-except FileNotFoundError: 
-    print("O arquivo não foi encontrado.")
+            nome, idade = paciente.strip().split(",")
+            lista.append(paciente)
+    for paciente in lista:
+        paciente.exibir_dados()
+except FileNotFoundError:
+    print("Erro.")
+
+
+
