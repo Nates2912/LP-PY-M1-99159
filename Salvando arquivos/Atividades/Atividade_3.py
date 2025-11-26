772f52a0ca77745f3859e42fbeaeabd554de57de
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-11-26 13:51:24 -0300

    feat: 26/11/2025

--- Salvando arquivos/Atividades/Atividade_3.py
+++ Salvando arquivos/Atividades/Atividade_3.py
@@ -9,6 +9,9 @@ class Funcionario:
     matricula: str
     endereco: str
 
+    def exibir_dados(self):
+        print(f"==FUNCIONÁRIOS== \n Nome: {self.nome} \nData de admissão: {self.dataadmissao} \nMatrícula: {self.matricula} \nEndereço: {self.endereco} \n")
+
 @dataclass
 class Cliente:
     nome: str
@@ -16,40 +19,47 @@ class Cliente:
     endereco: str
 
     def exibir_dados(self):
-        print(f"Nome: {self.nome} \nData de nascimento: {self.data} \nRG: {self.rg} \nCPF: {self.cpf} \n")
+        print(f"==FUNCIONÁRIOS== \n Nome: {self.nome} \nData de nascimento: {self.data_aniversario} \nEndereço: {self.endereco}\n")
 
 lista_de_funcionarios = []
 lista_de_clientes = []
-QUANTIDADE_FUNCIONARIOS = 2
-QUANTIDADE_CLIENTES= 2
+QUANTIDADE_FUNCIONARIOS = 1
+QUANTIDADE_CLIENTES= 1
 
 for i in range (QUANTIDADE_FUNCIONARIOS):
     funcionario = Funcionario(
-    nome= input ("Digite seu nome: "), dataadmissao= int(input("Digite sua data de admissão: ")), matricula= int(input("Digite sua matricula: ")), endereco= ("Digite seu endereço: ")
+    nome= input ("Digite seu nome: "), dataadmissao= input("Digite sua data de admissão: "), matricula= ("Digite sua matricula: "), endereco= ("Digite seu endereço: ")
     )
     lista_de_funcionarios.append(funcionario)
-    
+
     cliente = Cliente(
     nome= input ("Digite seu nome: "), data_aniversario = ("Digite sua data de admissão: "), endereco= ("Digite seu endereço: ")
     )
     lista_de_clientes.append(cliente)
 
+#criaçao do arquivo
+
     nome_do_arquivo = "funcionarios.csv"
     with open(nome_do_arquivo, "a") as arquivos_funcionarios:
         for funcionario in lista_de_funcionarios:
-            arquivos_funcionarios.write(f"{funcionario.nome}, {funcionario.data}, {funcionario.rg}, {funcionario.cpf}\n")
+            arquivos_funcionarios.write(f"{funcionario.nome}, {funcionario.dataadmissao}, {funcionario.matricula}, {funcionario.endereco}\n")
+            print("Dados salvos com sucesso.")
+    nome_do_arquivo2 = "clientes.csv"
+    with open(nome_do_arquivo2, "a") as arquivos_clientes:
+        for cliente in lista_de_clientes:
+            arquivos_clientes.write(f"{cliente.nome}, {cliente.dataadmissao}, {cliente.matricula}, {cliente.endereco}\n")
             print("Dados salvos com sucesso.")
 
-print("\nExibindo todos os funcionarios: ")
-lista = []
-try: 
-    with open (nome_do_arquivo, "r") as arquivo:
-        lista_todos_funcionarios = arquivo.readlines()
-        for funcionario in lista_todos_funcionarios:
-            nome, data, rg, cpf = funcionario.strip().split(",")
-            dados_funcionario = Funcionario(nome=nome, data=data, rg=rg, cpf=cpf)
-            lista.append(dados_funcionario)
-    for funcionario in lista:
-        funcionario.exibir_dados()
-except FileNotFoundError:
-    print("Erro.")
+# print("\nExibindo todos os funcionarios: ")
+# lista = []
+# try: 
+#     with open (nome_do_arquivo, "r") as arquivo:
+#         lista_todos_funcionarios = arquivo.readlines()
+#         for funcionario in lista_todos_funcionarios:
+#             nome, dataadmissao, matricula, endereco = funcionario.strip().split(",")
+#             dados_funcionario = Funcionario(nome=nome, dataadmissao=dataadmissao, matricula=matricula, endereco=endereco)
+#             lista.append(dados_funcionario)
+#     for funcionario in lista:
+#         funcionario.exibir_dados()
+# except FileNotFoundError:
+#     print("Erro.")
